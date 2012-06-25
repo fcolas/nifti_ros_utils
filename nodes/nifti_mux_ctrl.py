3b86842eba74f871ac0a8b8dc6cfce2f42114b81
Francis Colas <francis.colas@mavt.ethz.ch>
2012-06-25 09:43:35 +0000

    ETHZ: timeout to auto release lock
    git-svn-id: https://subversion.dfki.de/nifti/code/ros/stacks/nifti_misc/trunk/nifti_teleop@6226 3189491a-1924-0410-a745-03ab4139b441

--- nodes/nifti_mux_ctrl.py
+++ nodes/nifti_mux_ctrl.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 import roslib; roslib.load_manifest('nifti_teleop')
 import rospy
+import rospy.rostime as rostime
 from topic_tools.srv import MuxSelect
 from nifti_teleop.srv import *
 from std_msgs.msg import String
+from geometry_msgs.msg import Twist
 
 from threading import Lock
 from time import sleep, time
@@ -43,6 +45,20 @@ class NiftiMuxCtrl(object):
 		# monitor actual mux state
 		rospy.Subscriber('/mux_cmd_vel/selected', String, self.selected_cb)
 
+		# last messages
+		topics = priority.keys()
+		topics.remove('__none')
+		self.last_msg = dict(zip(topics, [rostime.Time(0)]*len(topics)))
+		for topic in topics:
+			rospy.Subscriber(topic, Twist, self.cmd_vel_cb_factory(topic))
+
+
+	def cmd_vel_cb_factory(self, topic):
+		def cb(msg):
+			if topic in self.stack:
+				self.last_msg[topic] = rostime.get_rostime()
+		return cb
+
 
 	def handle_acquire(self, req):
 		resp = False
@@ -80,6 +96,7 @@ class NiftiMuxCtrl(object):
 				self.mux_select(self.requesting)
 			else:
 				self.stack.remove(req.topic)
+			self.last_msg[req.topic] = rostime.Time(0)
 			rospy.logdebug("Request released.")
 			resp = True
 		self.lock.release()
@@ -94,7 +111,29 @@ class NiftiMuxCtrl(object):
 		self.requesting = None
 		if self.stack[-1]!=self.selected:
 			self.stack.append(self.selected)
+		if msg.data != '__none':
+			self.last_msg[msg.data] = rostime.get_rostime()
 
+	def run(self):
+		r = rospy.Rate(5)
+		while not rospy.is_shutdown():
+			r.sleep()
+			now = rostime.get_rostime()
+			for topic, last in self.last_msg.items():
+				if (not last.is_zero()) and ((now-last).to_sec()>2):
+					# auto release lock
+					self.lock.acquire()
+					while self.requesting:
+						sleep(0.010)
+					rospy.logwarn('Timeout release of request for "%s".', topic)
+					if self.stack[-1] == topic:
+						self.stack.pop()
+						self.requesting = self.stack[-1]
+						self.mux_select(self.requesting)
+					else:
+						self.stack.remove(topic)
+					self.last_msg[topic] = rostime.Time(0)
+					self.lock.release()
 
 
 ## Create a ROS node and instantiate the class.
@@ -103,7 +142,7 @@ def main():
 	try:
 		rospy.init_node('nifti_mux_control')
 		nmc = NiftiMuxCtrl()
-		rospy.spin()
+		nmc.run()
 	except rospy.ROSInterruptException:
 		pass
 
